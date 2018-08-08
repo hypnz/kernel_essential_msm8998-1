5e88df3331fc61deafc85eab3145ff5bfb87bd82
Pavankumar Kondeti <pkondeti@codeaurora.org>
2018-08-08 15:22:52 +0530

    sched: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: Ib93a77c1d12574354213a37b1171a462b7cab685
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1031,6 +1031,10 @@ static inline void dequeue_task(struct rq *rq, struct task_struct *p, int flags)
 		sched_info_dequeued(rq, p);
 
 	p->sched_class->dequeue_task(rq, p, flags);
+#ifdef CONFIG_SCHED_WALT
+	if (p == rq->ed_task)
+		early_detection_notify(rq, ktime_get_ns());
+#endif
 }
 
 void activate_task(struct rq *rq, struct task_struct *p, int flags)
